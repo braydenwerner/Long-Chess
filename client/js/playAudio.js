89b17a41257fb41eae474c029fe8ea0813ea4243
Brayden Werner <braydenwerner0@gmail.com>
2020-11-08 16:19:24 -0800

    Updated menu music

--- client/js/playAudio.js
+++ client/js/playAudio.js
@@ -2,7 +2,7 @@ import { getAudio } from "./assetLoader.js";
 
 const AUDIO = getAudio();
 
-const menuMusic = AUDIO["menu-music.wav"].cloneNode();
+const menuMusic = AUDIO["menu-music.mp3"].cloneNode();
 export function playMenuMusic() {
     menuMusic.volume = ".4";
     menuMusic.play();
@@ -13,19 +13,19 @@ export function pauseMenuMusic() {
 }
 
 export function gameStart() {
-    const gameStart = AUDIO["game-start.wav"].cloneNode();
+    const gameStart = AUDIO["game-start.mp3"].cloneNode();
     gameStart.volume = ".4";
     gameStart.play();
 }
 
 export function movePiece() {
-    const moveSelf = AUDIO["move-self.wav"].cloneNode();
+    const moveSelf = AUDIO["move-self.mp3"].cloneNode();
     moveSelf.volume = ".4";
     moveSelf.play();
 }
 
 export function illegalMove() {
-    const illegalMove = AUDIO["illegalMove.wav"].cloneNode();
+    const illegalMove = AUDIO["illegalMove.mp3"].cloneNode();
     illegalMove.volume = ".4";
     illegalMove.play();
 }
